4543151e0c4a21826f64908186ea1a3b3d5f2685
Sigsign <sig@signote.cc>
2024-06-29 18:05:04 +0900

    [WIP] Split rules

---
+++ src/offensive.rb
@@ -0,0 +1,9 @@
+# uBlacklist DSL file
+# - Author: Sigsign <sig@signote.cc>
+# - License: MIT-0
+
+# 2024-06-27:
+block "suki-kira.com"
+
+# 2024-06-27:
+block "tuber-review.com"
---
+++ src/privacy.rb
@@ -0,0 +1,7 @@
+# uBlacklist DSL file
+# - Author: Sigsign <sig@signote.cc>
+# - License: MIT-0
+
+# 2024-06-27:
+block "aidoly.net"
+block "ranky-ranking.net"
