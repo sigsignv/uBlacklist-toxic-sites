3c941ae11b6c76356dbf6b714aeb65e1d7aa039c
Sigsign <sig@signote.cc>
2024-06-29 15:54:52 +0900

    [WIP] Split rules

---
+++ src/flamebait.rb
@@ -0,0 +1,16 @@
+# uBlacklist DSL file
+# - Author: Sigsign <sig@signote.cc>
+# - License: MIT-0
+
+# 2024-06-27:
+block "ytranking.net" # redirect to yutura.net
+block "yutura.net"
+
+# 2024-06-27:
+block "logtube.jp"
+
+# 2024-06-27:
+block "myjitsu.jp"
+
+# 2024-06-27:
+block "p2y.jp"
