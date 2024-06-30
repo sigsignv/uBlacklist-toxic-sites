0466b77b63725cbb07cd62e6c651be0775bf3449
Sigsign <sig@signote.cc>
2024-06-30 23:33:43 +0900

    [WIP] Split rules

--- src/privacy.rb
+++ src/privacy.rb
@@ -2,6 +2,9 @@
 # - Author: Sigsign <sig@signote.cc>
 # - License: MIT-0
 
-# 2024-06-27:
+# 2024-06-27: ca-pub-7661335566848256
 block "aidoly.net"
 block "ranky-ranking.net"
+
+# 2024-06-27: wp:keni80
+block "aokiin.com"
