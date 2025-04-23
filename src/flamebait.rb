e9ddf521480767d033807d5e42593edcf8daf773
Sigsign <sig@signote.cc>
2025-04-23 10:29:30 +0900

    Split rules

--- src/flamebait.rb
+++ src/flamebait.rb
@@ -14,3 +14,16 @@ block "myjitsu.jp"
 
 # 2024-06-27:
 block "p2y.jp"
+
+# site is closed, but it still appears in search results
+block "budou-game.com"
+block "comisoku.com"
+block "gotube.info"
+block "jeep-m.jp"
+block "kurojou.com"
+block "netsoku.com"
+block "vnews.work"
+block "vrtuber-news.com"
+block "vtuber.matomemato.me"
+block "vtuber-labo.com"
+block "youtube5ch.com"
--- src/privacy.rb
+++ src/privacy.rb
@@ -12,3 +12,15 @@ block "aokiin.com"
 # 2024-06-27: ca-pub-9330239808282484
 block "minorie.com"
 block "dailytopic.jp"
+
+# site is closed, but it still appears in search results
+block "documentation-provence.org"
+block "idahotvep.org"
+block "iketog.com"
+block "mitsuya-magazine.com"
+block "nicolehappiness.com"
+block "nicotubers.com"
+block "nyfco.org"
+block "prospectmedicalholdings.com"
+block "tuberz.net"
+block "youtubernews.net"
