3a161d307e685ffd82817cb11d178c2f94bf7857
Sigsign <sig@signote.cc>
2025-04-23 15:48:59 +0900

    Split rules

--- src/offensive.rb
+++ src/offensive.rb
@@ -1,4 +1,4 @@
-# uBlacklist DSL file
+# sigsignv/uBlacklist-toxic-sites: offensive.rb
 # - Author: Sigsign <sig@signote.cc>
 # - License: MIT-0
 
@@ -7,3 +7,12 @@ block "suki-kira.com"
 
 # 2024-06-27:
 block "tuber-review.com"
+
+# 2024-06-27:
+block "tohyotalk.com"
+
+# 2024-06-27:
+block_path "note.com/ppoch/*"
+
+# 2024-06-27:
+block "psypuni7.com"
